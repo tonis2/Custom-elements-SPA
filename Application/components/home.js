57aa44bfeb48b8131c9ecc692e9c7c4bbeac3817
Tonis Anton <tonis.anton18@gmail.com>
2019-04-04 11:10:58 +0300

    Add comments

--- Application/components/home.js
+++ Application/components/home.js
@@ -6,11 +6,12 @@ export default class Home extends Element {
     async getPosts() {
        return await fetch("https://jsonplaceholder.typicode.com/posts").then(res => res.json())
     }
-
+    
+    //Open our post when we click on element
     open(id) {
         this.router.navigate(`/post/${id}`)
     }
-
+    
     async render() {
         const posts = await this.getPosts()
         return HTML`<section id="posts-container">
